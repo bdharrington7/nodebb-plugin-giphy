32390fa5b738dacf64cdf0080c462fb57b8fffab
bdharrington7 <BDHarrington7@gmail.com>
2016-09-25 17:15:07 -0500

    Add linting and fix style errors

--- library.js
+++ library.js
@@ -1,112 +1,113 @@
-/*global env:false */
+/* global env:false */
 
-'use strict';
+'use strict'
 
-var winston = module.parent.require('winston');
-var Settings = module.parent.require('./settings');
-var request = require('request');
+var winston = module.parent.require('winston')
+var Settings = module.parent.require('./settings')
+var request = require('request')
 
-var SocketPlugins = module.parent.require('./socket.io/plugins');
+var SocketPlugins = module.parent.require('./socket.io/plugins')
 var SocketAdmin = module.parent.require('./socket.io/admin')
 
-SocketPlugins.giphy = {};
+SocketPlugins.giphy = {}
 
-var Giphy = {};
+var Giphy = {}
 var ns = '[plugins/Giphy] '
-var debug;
-var apiUrl;
+var debug
+var apiUrl
 
 var constants = Object.freeze({
-    'name': 'Giphy',
-    'admin': {
-      'route': '/plugins/giphy',
-      'icon': 'fa-th-large',
-      'name': 'Giphy'
-    },
-    'namespace': 'nodebb-plugin-giphy'
-  });
+  'name': 'Giphy',
+  'admin': {
+    'route': '/plugins/giphy',
+    'icon': 'fa-th-large',
+    'name': 'Giphy'
+  },
+  'namespace': 'nodebb-plugin-giphy'
+})
 
-  var defaultSettings = {
-    strings: {
-      apiKey: 'dc6zaTOxFJmzC',
-      limit: '3',
-      parseMode: 'markdown'
-    }
+var defaultSettings = {
+  strings: {
+    apiKey: 'dc6zaTOxFJmzC',
+    limit: '3',
+    parseMode: 'markdown'
   }
-
-  var settings = new Settings('giphy', '0.0.1', defaultSettings, function () {
-    if (debug) {
-      winston.info(ns + 'Settings loaded')
-    }
-  })
-
-Giphy.onLoad = function (params, callback) {
-    // console.log('calling onLoad')
-    debug = env === 'development'
-    function render (req, res, next) {
-      res.render('admin/plugins/giphy')
-    }
-
-    params.router.get('/admin/plugins/giphy', params.middleware.admin.buildHeader, render)
-    params.router.get('/api/admin/plugins/giphy', render)
-    winston.info(ns + 'Initializing with API key:', settings.get('strings.apiKey'))
-    if (settings.get('strings.apiKey') === defaultSettings.strings.apiKey) {
-      winston.warn(ns + 'API key is still set to the public one, this may cause problems in production')
-    }
-    Giphy.init();
-    callback()
-  }
-
-Giphy.init = function() {
-  apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + settings.get('strings.apiKey');
-
-  if (settings.get('strings.limit')) {
-    apiUrl += '&limit=' + settings.get('strings.limit');
-  }
-  apiUrl += '&q=';
 }
 
-Giphy.parsePost = function(data, callback) {
-    if (!data || !data.postData || !data.postData.content) {
-        return callback(null, data);
+var settings = new Settings('giphy', '0.0.1', defaultSettings, function () {
+  if (debug) {
+    winston.info(ns + 'Settings loaded')
+  }
+})
+
+Giphy.onLoad = function (params, callback) {
+  debug = env === 'development'
+  function render (req, res, next) {
+    res.render('admin/plugins/giphy')
+  }
+
+  params.router.get('/admin/plugins/giphy', params.middleware.admin.buildHeader, render)
+  params.router.get('/api/admin/plugins/giphy', render)
+  winston.info(ns + 'Initializing with API key:', settings.get('strings.apiKey'))
+  if (settings.get('strings.apiKey') === defaultSettings.strings.apiKey) {
+    winston.warn(ns + 'API key is still set to the public one, this may cause problems in production')
+  }
+  Giphy.init()
+  callback()
+}
+
+Giphy.init = function () {
+  apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + settings.get('strings.apiKey')
+
+  if (settings.get('strings.limit')) {
+    apiUrl += '&limit=' + settings.get('strings.limit')
+  }
+  apiUrl += '&q='
+}
+
+Giphy.parsePost = function (data, callback) {
+  if (!data || !data.postData || !data.postData.content) {
+    return callback(null, data)
+  }
+
+  Giphy.parseRaw(data.postData.content, function (err, content) {
+    if (err) {
+      return callback(err)
     }
 
-    Giphy.parseRaw(data.postData.content, function(err, content) {
-        if (err) {
-            return callback(err);
-        }
+    data.postData.content = content
+    callback(null, data)
+  })
+}
 
-        data.postData.content = content;
-        callback(null, data);
-    });
-};
+Giphy.parseRaw = function (content, callback) {
+  return callback(null, content)
+}
 
-Giphy.parseRaw = function(content, callback) {
-    return callback(null, content);
-};
-
-SocketPlugins.giphy.search = function(socket, data, callback) {
-    winston.info('data:', data);
-    var url = apiUrl + data.query;
-    if (debug) {
-      winston.info('query:', url);
+SocketPlugins.giphy.search = function (socket, data, callback) {
+  winston.info('data:', data)
+  var url = apiUrl + data.query
+  if (debug) {
+    winston.info('query:', url)
+  }
+  request.get(url, function (error, res, body) {
+    if (error) {
+      winston.error(ns + error)
+      return callback(error)
     }
-    request.get(url, function(error, res, body) {
-        if (error) {
-            winston.error(error);
-            return callback(error);
-        }
-        var links = [];
-        if (res.statusCode == 200) {
-            winston.info('sending image links:', JSON.parse(body)['data']);
-            links = JSON.parse(body).data.map(function(record) {
-                return '<img src=' + record.images.fixed_width_small.url + ' original=' + record.images.original.url +
-                ' term=' + data.query + '>';
-            })
-            winston.info('links:', links);
-        }
-        return callback(null, links);
-    });
+    var links = []
+    if (res.statusCode === 200) {
+      if (debug) {
+        winston.info('sending image links:', JSON.parse(body)['data'])
+      }
+      links = JSON.parse(body).data.map(function (record) {
+        return '<img src=' + record.images.fixed_width_small.url + ' original=' + record.images.original.url +
+        ' term=' + data.query + '>'
+      })
+      winston.info('links:', links)
+    }
+    return callback(null, links)
+  })
 }
 
 /* Admin functions */
@@ -129,4 +130,4 @@ SocketAdmin.settings.syncGiphy = function (data) {
   settings.sync(Giphy.init)
 }
 
-module.exports = Giphy;
+module.exports = Giphy
--- static/autofill.js
+++ static/autofill.js
@@ -1,46 +1,43 @@
-"use strict";
-/* globals socket, app, utils */
+/* globals socket $:false */
+'use strict'
 
+$(document).ready(function () {
+  $(window).on('composer:autocomplete:init', function (ev, data) {
+    var strategy = {
+      match: /\B&([^\s\n]*)?$/,
+      search: function (term, callback) {
+        if (!term || term.length < 2) {
+          console.log('term is too short')
+          return callback([])
+        }
+        console.log('searching for', term)
 
-$(document).ready(function() {
+        socket.emit('plugins.giphy.search', {query: term}, function (err, links) {
+          if (err) {
+            return callback([])
+          }
 
-	$(window).on('composer:autocomplete:init', function(ev, data) {
-		var subset;
-		var strategy = {
-			match: /\B&([^\s\n]*)?$/,
-			search: function (term, callback) {
-				if (!term || term.length < 2) {
-					console.log('term is too short');
-					return callback([]);
-				}
-				console.log('searching for', term);
+          // return an array of links
 
-				socket.emit('plugins.giphy.search', {query: term}, function(err, links) {
-					if (err) {
-						return callback([]);
-					}
+          callback(links)
+        })
+      },
+      index: 1,
+      replace: function (selected) {
+        // console.log('selected:', selected)
+        var ele = $.parseHTML(selected)
+        var orig = $(ele).attr('original')
+        var term = $(ele).attr('term')
+        return '![' + term + '](' + orig + ') '
+      },
+      cache: true
+    }
 
-					// return an array of links
+    data.strategies.push(strategy)
+  })
 
-					callback(links);
-				});
-			},
-			index: 1,
-			replace: function (selected) {
-				// console.log('selected:', selected)
-				var ele = $.parseHTML(selected);
-				var orig = $(ele).attr('original');
-				var term = $(ele).attr('term');
-				return '![' + term + '](' + orig + ') ';
-			},
-			cache: true
-		};
-
-		data.strategies.push(strategy);
-	});
-
-	$(window).on('action:composer.loaded', function(e, data) {
-		var composer = $('#cmp-uuid-' + data.post_uuid + ' .write');
-		composer.attr('data-giphy', '1');
-	});
-});
+  $(window).on('action:composer.loaded', function (e, data) {
+    var composer = $('#cmp-uuid-' + data.post_uuid + ' .write')
+    composer.attr('data-giphy', '1')
+  })
+})
